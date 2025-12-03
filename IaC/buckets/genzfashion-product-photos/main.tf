5b9449fe9cf26030272c624a3637905871339871
TaenAhammed <taenahammed7@gmail.com>
2025-12-03 23:17:44 +0600

    Add terraform backend config and enhance the cloud concept diagram

--- IaC/buckets/genzfashion-product-photos/main.tf
+++ IaC/buckets/genzfashion-product-photos/main.tf
@@ -1,15 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.23.0"
     }
   }
   required_version = "v1.14.0"
+
+  backend "s3" {
+    bucket       = "devops-course-tfstate-demo"
+    key          = "dev/storage/s3/genzfashion-product-photos"
+    region       = "ap-southeast-1"
+    profile      = "ido"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region  = "ap-southeast-1"
   profile = "ido"
 }
 
